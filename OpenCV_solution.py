d8d37a0e8504af7d77cb0eea06118e0f54623733
AlexandraJogas <124614529+AlexandraJogas@users.noreply.github.com>
2023-02-21 20:59:06 +0200

    Update OpenCV_solution.py

--- OpenCV_solution.py
+++ OpenCV_solution.py
@@ -1,10 +1,4 @@
 
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
 ########### Import necessary libraries ################
 
 import numpy as np
@@ -27,15 +21,15 @@ Google it and you will get to the correct page.
 http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_image_display/py_image_dis
 play.html
 """
-color_img = cv2.imread('Lenna.png')  # naumpy array pixel shel tmuna (512X512X3) 3=aruzim =channels= BGR blue,red,green
+color_img = cv2.imread('Lenna.png')  
 
 
 """
 5. Show the image and then keep the window open for 10 seconds (imshow, waitkey)
 """
-cv2.imshow('image',color_img)  # liftoah tmuna
-cv2.waitKey(10000)      # bemaalah riza tiftah 10 sec ve tisgor,  10000 milisec=10 sec, im =0 tehake ad she elhaz al kol kaftor
-cv2.destroyAllWindows() # lisgor halonit shel image she patahta
+cv2.imshow('image',color_img)  
+cv2.waitKey(10000)     
+cv2.destroyAllWindows() 
 
 
 """
@@ -43,8 +37,8 @@ cv2.destroyAllWindows() # lisgor halonit shel image she patahta
 window
 """
 cv2.imshow('image',color_img)
-cv2.waitKey()                 # liftoah halon ad she le lohzim euze she kaftor, 0= gam efshar lihtov
-cv2.destroyAllWindows()       # lisgor halonit shel image she patahta
+cv2.waitKey()                
+cv2.destroyAllWindows()       
 
 
 """
@@ -52,14 +46,14 @@ cv2.destroyAllWindows()       # lisgor halonit shel image she patahta
 http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_core/py_basic_ops/py_basic_ops.ht
 ml#basic-ops
 """
-px = color_img[100,100]  # ze numpy array, mazig ereh be makom (100,100) BGR [255,60,100]= ze gavan ve pixel 100
+px = color_img[100,100]  
 print(px)
 
 
 """
 8. Print only the blue value of this pixel
 """
-only_blue = color_img[100,100,0]    # lageshet le ereh kahol =255, 0=blue, 1=green, 2= red
+only_blue = color_img[100,100,0]   
 print(only_blue)
 #or
 print(px[0])
@@ -67,22 +61,22 @@ print(px[0])
 """
 9. Access this pixel value and then modify it’s value using the “item” and “itemset” functions
 """
-color_img.itemset((100,100,0),255)   # be makom (100,100,0)  leaziv ereh 255, laasot set le item
-color_img.item((100,100,0))          # leahzir item be makom (100,100,0)
+color_img.itemset((100,100,0),255)  
+color_img.item((100,100,0))          
 
 """
 10. Print the image shape, size and dtype values to the screen
 """
-print(color_img.shape)   # efshar leadpis shape, size, dtype kmo be numpy array  (512,512,3)
-print(color_img.size)    # (512,512,3)= 786.432  kama pixel or arahim mahilya tmuna
+print(color_img.shape)   
+print(color_img.size)    
 print(color_img.dtype)   # uint8= unsigned integer8
 
 """
 11. Choose a rectangluar area in the image, copy it’s values to a variable and then paste it
 in another area in the pixel
 """
-rect_area = color_img[120:180, 180:240]   # lehozia helek me tmuna
-color_img[120:180, 240:300] = rect_area   # ve lasim be koordinata aheret
+rect_area = color_img[120:180, 180:240]   
+color_img[120:180, 240:300] = rect_area  
 
 
 
@@ -90,19 +84,19 @@ color_img[120:180, 240:300] = rect_area   # ve lasim be koordinata aheret
 12. Split the RGB image into three different images: R,G,B and show them. Use the “split”
 function
 """
-b,g,r = cv2.split(color_img)  # lakahat image ve lehalek le 3 tmunot nifradot
+b,g,r = cv2.split(color_img)  
 
 
 """
 13. Merge these values back to a RGB image using the “merge” function
 """
-merged_img = cv2.merge((b,g,r))  # lakahat 3 tmunot ve lemazeg le tmuna ahat
+merged_img = cv2.merge((b,g,r))  
 
 
 """
 14. Do the same operation now with NumPy indexing of the image
 """
-b_idx = color_img[:,:,0]   # laasot split be ezrat numpy array, lakahat kol shurot, kol amudot ve channel blue
+b_idx = color_img[:,:,0]   
 g_idx = color_img[:,:,1]
 r_idx = color_img[:,:,2]
 
@@ -114,9 +108,9 @@ http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/copyMakeBorder/copyMak
 rder.html
 Use two differnet flags: BORDER_REPLICATE 2. BORDER_CONSTANT
 """
-red = [255,0,0]  # BGR                                # BORDER_REPLICATE= meshahpel pixels be zdadim ose oto davar, pixels krovim
-replicate = cv2.copyMakeBorder(color_img,10,10,10,10, cv2.BORDER_REPLICATE) # godel border 10,10,10,10=pixels bezdadim, povtorit, rastyanut 
-constant  = cv2.copyMakeBorder(color_img,10,10,10,10, cv2.BORDER_CONSTANT, value =red)  # border im ereh kavua, ramka krasnaya budet
+red = [255,0,0]  # BGR                              
+replicate = cv2.copyMakeBorder(color_img,10,10,10,10, cv2.BORDER_REPLICATE)  
+constant  = cv2.copyMakeBorder(color_img,10,10,10,10, cv2.BORDER_CONSTANT, value =red)  
 
 plt.imshow(color_img,'gray'),plt.title('Original')
 plt.imshow(replicate,'gray'),plt.title('Replicate')
@@ -125,37 +119,35 @@ plt.show()
 """
 16. Convert the RGB image into a Gray level image using “cvtColor” and “show”
 """
-img2gray = cv2.cvtColor(color_img,cv2.COLOR_BGR2GRAY)  # cvtColor= convert color, leyazer tmuna gray
-plt.imshow(img2gray)  # roim green ve le gray ki le katavnu plt.imshow(img2gray,cmap='gray')
-
+img2gray = cv2.cvtColor(color_img,cv2.COLOR_BGR2GRAY)  # cvtColor= convert color
+plt.imshow(img2gray)  
 
 """
 17. Create a binary image from the Gray image using “threshold” and show
-"""                                       # anu rozim THRESH_BINARY 2 option: black/white 0/255
-ret1, binary_img = cv2.threshold(img2gray, 150, 255, cv2.THRESH_BINARY) # 150 krai=saf eifo iani hoteh= hituh
-plt.imshow(binary_img, cmap='grey')                                     # 255 ma ihie ereh gavoa = ma ani mazeva be ereh ahi gavoa
+"""                                    
+ret1, binary_img = cv2.threshold(img2gray, 150, 255, cv2.THRESH_BINARY) 
+plt.imshow(binary_img, cmap='grey')                                    
 
 
 """
 18. Blur the image using “GaussianBlur” and show
 """
-blurred_img = cv2.GaussianBlur(color_img,(7,7),3)  # osim convolution shel tmuna keasher (7,7)=filtr=karnel zura gausian, 3=sigma gausian
-plt.imshow(blurred_img)             # filter Gaussiany ose tishtush le tmuna, sigma gausiian ma ihie efresh arahim bein emza le misasaviv
+blurred_img = cv2.GaussianBlur(color_img,(7,7),3)  
+plt.imshow(blurred_img)             
 
 """
 19. Create a Gaussian kernel and filter using “filter2D”
 """
-GaussKernel=cv2.getGaussianKernel((10,10),20)  # zura shel filtr gaussian, naziv filter gaussiany le filter2D, 10X10 godel filter, 20=sigma std
-filt_2d = cv2.filter2D(color_img, -1, GaussKernel)   # filter du meimadi= filter2D, -1= input shave le output be sug tmuna
-plt.imshow(filt_2d)                         # 20= sigma kovea distribution shel arahim
+GaussKernel=cv2.getGaussianKernel((10,10),20) 
+filt_2d = cv2.filter2D(color_img, -1, GaussKernel)   
+plt.imshow(filt_2d)                        
 
 """
 20. Create a three level pyramid using “pyrDown” and “pyrUp”
 """
-pyrdown = cv2.pyrDown(color_img)   # pyramida= kshe rozim lehistakel al scale tmunot be bat ahat
+pyrdown = cv2.pyrDown(color_img)   
 plt.imshow(pyrdown)
-pyrdown.shape   # (256,256,1) tmuna ktana yoter
-
+pyrdown.shape   
 plt.imshow(color_img)
 
 pyrup = cv2.pyrUp(color_img)
@@ -177,9 +169,9 @@ plt.imshow(resize_2)
 """
 # nagdir matriza sivuv ve azaza
 M = np.array([[0., .5, 1.],
-              [.5, 0,  1.]])  # laasot sivuv, ahaza, matiha: matriza (2,2) sivuv, (2,1) azaza
-# laasot sivuv, ahaza, matiha le tmuna
-aff_trans = cv2.warpAffine(color_img, M, (300,300))  # ze resize (300,300) kama pixels ani roza be tozaa
+              [.5, 0,  1.]]) 
+
+aff_trans = cv2.warpAffine(color_img, M, (300,300)) 
 plt.imshow(aff_trans)
 
 
@@ -189,7 +181,7 @@ the affine transofrm using warpAffine
 """
 angle = 74
 angle_in_rad = angle*np.pi/180
-rotation_mat = cv2.getRotationMatrix2D((50,50), angle_in_rad, 0.5)  # nagdir matriza sivuv ve azaza
-aff_trans_2 = cv2.warpAffine(color_img, rotation_mat, (300,300))    # laasot sivuv, ahaza, matiha le tmuna
+rotation_mat = cv2.getRotationMatrix2D((50,50), angle_in_rad, 0.5)  
+aff_trans_2 = cv2.warpAffine(color_img, rotation_mat, (300,300))    
 plt.imshow(aff_trans_2)
 
